546b0be11741f467ebc3e49eb84afa76ad29a5a0
olegmazhanov <oleg.mazhanov@gmail.com>
2018-07-17 12:34:05 +1200

    Show/Hide added

---
+++ ClassicCinema/showHide.js
@@ -0,0 +1,29 @@
+function showHideDetails() {
+    //alert("Not implemented yet");
+    var paragraphs, p;
+    paragraphs = this.parentNode.getElementsByTagName("p");
+    //paragraphs = this.childNodes;
+    for (p = 0; p < paragraphs.length; p+=1) {
+
+        if (paragraphs[p].style.display === "none") {
+            paragraphs[p].style.display = "block";
+            this.style.color = "black";
+        } else {
+            paragraphs[p].style.display = "none";
+            this.style.color = "red";
+        }
+    }
+
+}
+function setup() {
+    var films, f, title;
+    films = document.getElementsByClassName("film");
+    for (f = 0; f < films.length; f+=1) {
+        title = films[f].getElementsByTagName("h3")[0];
+        title.onclick = showHideDetails;
+        title.style.cursor = "pointer";
+    }
+}
+if (document.getElementById) {
+    window.onload = setup;
+}
