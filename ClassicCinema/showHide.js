49279953c7504f2539f66467b689cf96af3976a6
omazhanov <oleg.mazhanov@gmail.com>
2018-07-18 11:20:20 +1200

    Corrections during LAB

--- ClassicCinema/showHide.js
+++ ClassicCinema/showHide.js
@@ -1,32 +1,37 @@
 
 function showHideDetails() {
     "use strict";
-    var paragraphs , p;
+    var paragraphs , p, image,images;
 
     //alert("Not implemented yet");
     paragraphs = this.parentNode.getElementsByTagName("p");
+    images = this.parentNode.getElementsByTagName("img");
+
     //paragraphs = this.childNodes;
     for (p = 0; p < paragraphs.length; p+=1) {
 
         if (paragraphs[p].style.display === "none") {
             paragraphs[p].style.display = "block";
-            this.style.color = "black";
+            images[0].style.display = "block";
         } else {
-            paragraphs[p].style.display = "none";
-            this.style.color = "red";
+            paragraphs[p].style.display = "none"
+            images[0].style.display = "none";
         }
     }
-
 }
 function setup() {
     "use strict";
-    var films, f, title;
+    var films, f, title,images;
     films = document.getElementsByClassName("film");
     for (f = 0; f < films.length; f+=1) {
         title = films[f].getElementsByTagName("h3")[0];
         title.onclick = showHideDetails;
         title.style.cursor = "pointer";
+        images = films[f].getElementsByTagName("img")[0];
+        images.onclick = showHideDetails;
+        images.style.cursor = "pointer";
     }
+
 }
 if (document.getElementById) {
     window.onload = setup;
--- js33.js
+++ js33.js
@@ -1,19 +1,22 @@
-
+"use strict";
 function sayHello() {
-  var name;
-  var target;
-  name=document.getElementById("name").value;
-  if(name.length === 0 ){
-    name = " World";
-  }
-  target = document.getElementById("result");
-  target.innerHTML = "Hello, " + name + "!";
-  return false;
+    var name;
+    var target;
+    name=document.getElementById("name").value;
+    if(name.length === 0 ){
+      name = " World";
+    }
+    target = document.getElementById("result");
+    target.innerHTML = "Hello, " + name + "!";
+    return false;
 }
 function setup() {
-  var button;
-  button = document.getElementById("hello");
-  button.onclick = sayHello;
+    var button;
+    var form;
+    button = document.getElementById("hello");
+    button.onclick = sayHello;
+    form=document.getElementById("form");
+    form.onsubmit = sayHello;
 }
 if (document.getElementById) {
   window.onload = setup;
