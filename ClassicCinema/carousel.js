079d743afd4e192968356dd6ec5cdfa5bfc3a1d9
omazhanov <oleg.mazhanov@gmail.com>
2018-07-25 10:07:03 +1200

    start1

---
+++ ClassicCinema/carousel.js
@@ -0,0 +1,27 @@
+var imageList, imageIndex;
+function nextImage() {
+    var target;
+    target = document.getElementById("carousel");
+    target.innerHTML = "<img src=" + imageList[imageIndex] + ">";
+        //alert(test);
+        if (imageIndex < 2 ) {
+            imageIndex +=1;
+        }
+        else {
+            imageIndex = 0;
+        }
+
+}
+function setup() {
+    imageList = [];
+    imageList.push("images/Metropolis.jpg");
+    imageList.push("images/Plan_9_from_Outer_Space.jpg");
+    imageList.push("images/Vertigo.jpg");
+    imageIndex = 0;
+    nextImage();
+    setInterval(nextImage, 2000);
+}
+if (document.getElementById) {
+    window.onload = setup;
+
+}
