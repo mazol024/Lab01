aa8f26fe75982ba24053cd2b3e9b96506e62aaaf
olegmazhanov <oleg.mazhanov@gmail.com>
2018-07-29 23:24:36 +1200

    Cookies added.2

--- ClassicCinema/cookies.js
+++ ClassicCinema/cookies.js
@@ -10,16 +10,16 @@ var Cookie = (function () {
             expires = "";
         }
         /*document.cookie = name + "=" + value + expires + "; path=/";*/
-        document.cookie = JSON.stringify(encodeURIComponent(name + "=" + value + expires + "; path=/"));
+        document.cookie = encodeURIComponent(name + "=" + value + expires + "; path=/");
     };
     pub.get = function (name) {
         var nameEq, cookies, cookie, i;
-        nameEq = JSON.stringify(encodeURIComponent(name + "="));
+        nameEq = encodeURIComponent(name + "=");
         cookies = document.cookie.split(encodeURIComponent(";"));
         for (i = 0; i < cookies.length; i += 1) {
             cookie = cookies[i].trim();
             if (cookie.indexOf(nameEq) === 0) {
-                return JSON.parse(decodeURIComponent(cookie.substring(nameEq.length, cookie.length)));
+                return decodeURIComponent(cookie.substring(nameEq.length, cookie.length));
             }
         }
         return null;
