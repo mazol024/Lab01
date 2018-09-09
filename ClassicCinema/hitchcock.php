83a015e1b1f5fb92cfcc7a8c4e50c7aa17e58d20
olegmazhanov <oleg.mazhanov@gmail.com>
2018-09-09 12:49:14 +1200

    Footer extracted to php file.

--- ClassicCinema/hitchcock.php
+++ ClassicCinema/hitchcock.php
@@ -117,7 +117,8 @@
 
         </main>
 
-        <footer>
+        <?php include ("footer.php");?>
+<!--        <footer>
             <p>
             Classic Cinema is not a real store. No products are available, and no money will be accepted.
             </p>
@@ -125,6 +126,6 @@
             All images from <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikimedia Commons</a> and are believed to be in the public domain.
             </p>
         </footer>
-
+-->
     </body>
 </html>
